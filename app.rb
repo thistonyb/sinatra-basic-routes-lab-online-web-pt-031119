4592bd703215e98db0ae2cb4262aa92e1624793c
Tony Baker <this.tonybaker@gmail.com>
2019-06-04 22:03:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Tony"
+  end
+
+  get '/hometown'
+    "My hometown is Seattle",
+  end
+
+  get '/favorite-song'
+    "My favorite song is anything by Tears for Fears".
+  end
 end
