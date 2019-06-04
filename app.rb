8414ecf7f69f2d8d2431d8c194fbff0df3ff9264
Tony Baker <this.tonybaker@gmail.com>
2019-06-04 22:07:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Tony"
+  end
+
+  get '/hometown' do
+    "My hometown is Seattle,"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is anything by Tears for Fears."
+  end
 end
